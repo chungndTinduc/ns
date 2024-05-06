ef3dcfab89b6ea37cb2290fa4e15120e84935a8b
chungnd <chungnd@tinducsoftware.com>
2024-05-06 17:09:05 +0700

    up

---
+++ down.ps1
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose.yml  down
+docker network rm nuocsach-network
---
+++ up.ps1
@@ -0,0 +1,4 @@
+docker network create nuocsach-network
+docker-compose -f docker-compose.yml  up -d
+
+
